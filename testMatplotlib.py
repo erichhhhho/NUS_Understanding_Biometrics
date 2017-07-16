c1a35c6fcda9ef7c91503338f18885c7f632d2b8
erichhhhho <961637239@qq.com>
2017-07-16 21:33:20 +0800

    test Video (OpenCV )

--- testMatplotlib.py
+++ testMatplotlib.py
@@ -6,6 +6,13 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("F:\Desktop\WeChat Image_20170708121829.jpg", cv2.IMREAD_COLOR)
 
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
+b,g,r = cv2.split(img)
+img2 = cv2.merge([r,g,b])
+plt.subplot(121);plt.imshow(img) # expects distorted color
+plt.subplot(122);plt.imshow(img2) # expect true color
 plt.show()
+
+cv2.imshow('bgr image',img) # expects true color
+cv2.imshow('rgb image',img2) # expects distorted color
+cv2.waitKey(0)
+cv2.destroyAllWindows()
